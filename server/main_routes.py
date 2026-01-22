0a697994b194101c2270355c2233293e63b0ef5e
Phuluso <119367220+PS-ThePhantom@users.noreply.github.com>
2026-01-22 22:41:29 +0200

    feat: apply page initial inputs

--- server/main_routes.py
+++ server/main_routes.py
@@ -1,14 +1,15 @@
-from flask import Blueprint, render_template, jsonify
+from flask import Blueprint, render_template
+import os
 
 main_routes = Blueprint('main', __name__)
 
 @main_routes.route("/")
 def index():
-    return render_template("index.html"), 200
+    return render_template("index.html", session_length=os.getenv("BOOKING_MINUTES", "30")), 200
 
 @main_routes.route("/apply")
 def apply():
-    return render_template("apply.html"), 200
+    return render_template("apply.html", session_length=os.getenv("BOOKING_MINUTES", "30")), 200
 
 @main_routes.route("/<path:path>")
 def not_exist(path):
